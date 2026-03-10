5ee6c640239bd45feffc60c7db702d71bd17df40
NatheerAyyan76 <natheeray07@gmail.com>
2026-03-10 11:51:23 +0300

    Build front-end for GitHub Pages

--- client/front_end/vite.config.js
+++ client/front_end/vite.config.js
@@ -16,14 +16,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-   plugins: [react()],
-   server: {
-     proxy: {
+  base: '/LandBook/',   // GitHub Pages
+  plugins: [react()],
+  server: {
+    proxy: {
       '/api': {
-       target: 'https://landbook.onrender.com',
-       changeOrigin: true,
-        secure: false
-     }
-     }
-   }
+        target: 'https://landbook.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
